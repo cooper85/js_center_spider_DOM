518eb33a9b444e721ece5181d855f2ed53027add
Sergey Zubinskiy <sergey.zubinskiy@skynix.co>
2025-09-06 01:40:26 +0300

    task implementation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+/**
+ * Center the spider
+ *
+ * @param {string} fieldClass
+ * @param {string} spiderClass
+ */
+function centerTheSpider(fieldClass, spiderClass) {
+  const wall = document.querySelector(fieldClass);
+  const spider = document.querySelector(spiderClass);
+
+  const offsetX = (wall.clientWidth - spider.clientWidth) / 2;
+  const offsetY = (wall.clientHeight - spider.clientHeight) / 2;
+
+  spider.style.left = `${offsetX}px`;
+  spider.style.top = `${offsetY}px`;
+}
+
+centerTheSpider('.wall', '.spider');
